2d88a0e3f391849db6af66218c15b26707566e63
hanum <rntls123@naver.com>
2023-08-16 00:33:05 +0900

    Fix(App): Wrong import ThemeProvider

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { preventAutoHideAsync, hideAsync } from "expo-splash-screen";
-import { NavigationContainer, ThemeProvider } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import Root from "./src/navigation/Root";
 import { useFonts } from "expo-font";
 import { AntDesign } from "@expo/vector-icons";
 import { defaultTheme } from "./styles/theme";
+import { ThemeProvider } from "styled-components/native";
 
 preventAutoHideAsync();
 export default function App() {
