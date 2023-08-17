a59a5d33a0fe961e1cf49960eea0373e32f0fe99
hanum <rntls123@naver.com>
2023-08-18 00:10:14 +0900

    Feat(PageLayout): Add PageLayout atom

---
+++ src/components/atoms/View/PageLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components/native";
+import { BaseViewContainer } from "./BaseView";
+
+interface IPageLayout {
+  children?: any;
+}
+const Container = styled(BaseViewContainer)`
+  padding: 20px;
+`;
+const PageLayout: React.FC<IPageLayout> = ({ children }) => {
+  return <Container>{children}</Container>;
+};
+
+export default PageLayout;
