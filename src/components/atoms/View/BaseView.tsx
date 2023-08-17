9e7951fa126103ecbc80cacdffb8495a8d48aab7
hanum <rntls123@naver.com>
2023-08-18 00:08:34 +0900

    Style(BaseView): Typo

--- src/components/atoms/View/BaseView.tsx
+++ src/components/atoms/View/BaseView.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components/native";
 
-interface BaseView {
+interface IBaseView {
   children?: any;
 }
 const Container = styled.View`
   background-color: ${(props) => props.theme.bgColor};
 `;
-const BaseView: React.FC<BaseView> = ({ children }) => {
+const BaseView: React.FC<IBaseView> = ({ children }) => {
   return <Container>{children}</Container>;
 };
 
