dbabe200ff0e589aedb03896ab2f1c3506ced2b6
hanum <rntls123@naver.com>
2023-08-20 21:16:26 +0900

    Feat(Landing): Add Landing page

--- src/components/atoms/View/PageLayout.tsx
+++ src/components/atoms/View/PageLayout.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styled from "styled-components/native";
-import { BaseViewContainer } from "./BaseView";
+import { BaseViewContainer, IBaseView } from "./BaseView";
 
-interface IPageLayout {
-  children?: any;
-}
 const Container = styled(BaseViewContainer)`
   padding: 20px;
   flex: 1;
 `;
-const PageLayout: React.FC<IPageLayout> = ({ children }) => {
-  return <Container>{children}</Container>;
+const PageLayout: React.FC<IBaseView> = ({ children, style }) => {
+  return <Container style={style}>{children}</Container>;
 };
 
 export default PageLayout;
--- src/components/views/auths/Landing.tsx
+++ src/components/views/auths/Landing.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import PageLayout from "../../atoms/View/PageLayout";
-import BaseText from "../../atoms/Texts/BaseText";
+import MainLogo from "../../molecules/Logos/MainLogo";
+import BaseCard from "../../molecules/Cards/BaseCard";
+import HCenterView from "../../atoms/View/HCenterView";
 
 const Landing = () => {
   return (
     <PageLayout>
-      <BaseText>Landing</BaseText>
+      <HCenterView style={{ marginTop: 60 }}>
+        <BaseCard cardType="largeSqaure" trafficLight={true}>
+          <MainLogo />
+        </BaseCard>
+      </HCenterView>
     </PageLayout>
   );
 };
