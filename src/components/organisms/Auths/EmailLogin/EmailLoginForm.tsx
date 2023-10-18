37ca30cfa66ed158dcc1097ea3f5bf401616245a
hanum <rntls123@naver.com>
2023-10-18 16:53:26 +0700

    Feat(EmailLoginForm): Remove Password confirm

--- src/components/organisms/Auths/EmailLogin/EmailLoginForm.tsx
+++ src/components/organisms/Auths/EmailLogin/EmailLoginForm.tsx
@@ -8,28 +8,24 @@ import { useTheme } from "styled-components";
 
 interface IEmailLoginForm {
   email: string;
-  password1: string;
-  password2: string;
+  password: string;
 }
 const EmailLoginForm = () => {
   const theme = useTheme();
   const emailInputRef = useRef<TextInput>(null);
-  const password1InputRef = useRef<TextInput>(null);
-  const password2InputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
   const {
     control,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<IEmailLoginForm>({
     defaultValues: {
       email: "",
-      password1: "",
-      password2: "",
+      password: "",
     },
   });
-  const onSubmit = (data: IEmailLoginForm) => {
-    console.log("Exec");
-  };
+  const onSubmit = (data: IEmailLoginForm) => {};
   useEffect(() => {
     emailInputRef.current?.focus();
   }, []);
@@ -60,12 +56,16 @@ const EmailLoginForm = () => {
             returnKeyType="next"
             value={value}
             onChangeText={onChange}
-            onSubmitEditing={() => password1InputRef.current?.focus()}
+            onSubmitEditing={() => passwordInputRef.current?.focus()}
           />
         )}
         name="email"
       />
-      {errors.email && <BaseText>{errors.email.message}</BaseText>}
+      {errors.email && (
+        <BaseText size={"small"} style={{ color: "red", marginTop: 10 }}>
+          {errors.email.message}
+        </BaseText>
+      )}
       <Controller
         control={control}
         rules={{
@@ -73,7 +73,7 @@ const EmailLoginForm = () => {
         }}
         render={({ field: { onChange, onBlur, value } }) => (
           <LabeledTextInput
-            textInputRef={password1InputRef}
+            textInputRef={passwordInputRef}
             label="PW"
             placeholder="비밀번호를 입력하세요."
             style={{
@@ -91,37 +91,16 @@ const EmailLoginForm = () => {
             onChangeText={onChange}
             onBlur={onBlur}
             value={value}
-            onSubmitEditing={() => password2InputRef.current?.focus()}
-          />
-        )}
-        name="password1"
-      />
-      <Controller
-        control={control}
-        rules={{
-          required: true,
-        }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <LabeledTextInput
-            textInputRef={password2InputRef}
-            label=""
-            placeholder="비밀번호 확인"
-            style={{
-              borderColor: theme.headerColor,
-              width: 240,
-            }}
-            containerStyle={{ backgroundColor: theme.headerColor, width: 300 }}
-            size="small"
-            textContentType="oneTimeCode"
-            secureTextEntry={true}
-            onChangeText={onChange}
-            onBlur={onBlur}
-            value={value}
             onSubmitEditing={handleSubmit(onSubmit)}
           />
         )}
-        name="password2"
+        name="password"
       />
+      {errors.password && (
+        <BaseText size={"small"} style={{ color: "red", marginTop: 10 }}>
+          {errors.password.message}
+        </BaseText>
+      )}
     </BaseView>
   );
 };
