cd429d4ce34d68f8eb330ee90c0f814e155de6e9
hanum <rntls123@naver.com>
2023-10-25 19:54:12 +0700

    Feat(QuizPlayButton): Add QuizPlayButton in QuizList screen

---
+++ src/components/molecules/Buttons/Quizzes/QuizPlayButton.tsx
@@ -0,0 +1,45 @@
+import BaseButton, { IBaseButton } from "components/atoms/Buttons/BaseButton";
+import GradientCard from "components/atoms/Quizzes/GradientCard";
+import React from "react";
+import styled from "styled-components/native";
+
+const GRADIENT_COLORS = [
+  "#ACD15E",
+  "#FF9246",
+  "#F45343",
+  "#9575B3",
+  "#ACD15E",
+  "#FF9246",
+  "#F45343",
+  "#9575B3",
+];
+const Btn = styled(BaseButton)`
+  flex: 1;
+  margin-right: 10px;
+  height: 70px;
+`;
+const TextContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+`;
+const PlayText = styled.Text`
+  font-size: 36px;
+  font-weight: 700;
+`;
+const QuizPlayButton = ({ onPress }: IBaseButton) => {
+  return (
+    <Btn onPress={onPress}>
+      <>
+        <GradientCard colors={GRADIENT_COLORS} />
+        <TextContainer>
+          <PlayText>play();</PlayText>
+        </TextContainer>
+      </>
+    </Btn>
+  );
+};
+
+export default QuizPlayButton;
