3ffc0fe4998f58120abbdc5c6f6f44b6488c3ad2
hanum <rntls123@naver.com>
2023-11-08 17:48:36 +0700

    Design(QuizChoiceButton): Change choice btn styles

--- src/components/atoms/Buttons/QuizChoiceButton.tsx
+++ src/components/atoms/Buttons/QuizChoiceButton.tsx
@@ -15,16 +15,27 @@ interface IQuizChoiceButton extends IBaseButton {
   choiceText: string;
 }
 const Btn = styled(BaseButton)<{ state: quizBtnState }>`
-  border: 0.5px;
-  border-color: ${(props) => props.theme.textColor};
+  border: 0.2px;
+  border-color: ${(props) =>
+    props.state === quizBtnState.correct
+      ? props.theme.green
+      : props.state === quizBtnState.wrong
+      ? props.theme.warning
+      : props.theme.textColor};
   background-color: ${(props) =>
     props.state === quizBtnState.correct
       ? props.theme.green
       : props.state === quizBtnState.wrong
       ? props.theme.warning
       : props.theme.bgColor};
-  border-radius: 5px;
-  margin-bottom: 10px;
+  border-radius: 50px;
+  margin-bottom: 15px;
+`;
+const BtnText = styled(BaseText)<{ state: quizBtnState }>`
+  color: ${(props) =>
+    props.state === quizBtnState.correct || props.state === quizBtnState.wrong
+      ? "black"
+      : "white"};
 `;
 const QuizChoiceButton = ({
   index,
@@ -42,7 +53,9 @@ const QuizChoiceButton = ({
       : quizBtnState.wrong;
   return (
     <Btn state={state} {...props} style={{ padding: 20 }}>
-      <BaseText size="small">{choiceText}</BaseText>
+      <BtnText state={state} size="small">
+        {choiceText}
+      </BtnText>
     </Btn>
   );
 };
