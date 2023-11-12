adb719c3ccb34c59e1e9d9f433124fff3608a7e4
hanum <rntls123@naver.com>
2023-11-12 21:13:09 +0700

    Feat(BaseAnimatedView): Add BaseAnimatedView

---
+++ src/components/atoms/View/BaseAnimatedView.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Animated, ViewProps } from "react-native";
+import styled from "styled-components/native";
+
+export interface IBaseAnimatedView extends ViewProps {
+  children?: any;
+}
+export const BaseViewContainer = styled(Animated.View)`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+const BaseAnimatedView: React.FC<IBaseAnimatedView> = ({
+  children,
+  style,
+  ...props
+}) => {
+  return (
+    <BaseViewContainer style={style} {...props}>
+      {children}
+    </BaseViewContainer>
+  );
+};
+
+export default BaseAnimatedView;
