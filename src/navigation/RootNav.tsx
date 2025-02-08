8ee0d3524b8edc1cef45f9a820c0a6b856a12e5b
hanum <rntls123@naver.com>
2025-02-08 19:09:57 +0900

    Feat: Add initial ads

--- src/navigation/RootNav.tsx
+++ src/navigation/RootNav.tsx
@@ -4,15 +4,65 @@ import RootStackNav from "./RootStackNav";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useNavigation } from "@react-navigation/native";
 import { setNotificationListener } from "notifications/notification";
+import { getAdId } from "ads/getAdId";
+import mobileAds, { useInterstitialAd } from "react-native-google-mobile-ads";
+import { SHOW_INITIAL_AD } from "ads/ad-flag";
+import {
+  getTrackingPermissionsAsync,
+  requestTrackingPermissionsAsync,
+  PermissionStatus,
+} from "expo-tracking-transparency";
+import { ActivityIndicator } from "react-native";
+import { CenterViewContainer } from "components/atoms/View/CenterView";
 
 const NativeStack = createNativeStackNavigator();
 
 const RootNav = () => {
+  // load ad
+  const adId = getAdId("INTERSTITIAL");
+  const { isLoaded, isClosed, isClicked, error, show, load } =
+    useInterstitialAd(adId);
+  useEffect(() => {
+    if (adId !== null && SHOW_INITIAL_AD) {
+      load();
+    }
+  }, [load]);
+
+  useEffect(() => {
+    if (isLoaded && SHOW_INITIAL_AD) {
+      show();
+    }
+  }, [isLoaded, show]);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const { status } = await getTrackingPermissionsAsync();
+        if (status === PermissionStatus.UNDETERMINED) {
+          await requestTrackingPermissionsAsync();
+        }
+        if (SHOW_INITIAL_AD) {
+          await mobileAds().initialize();
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    init();
+  }, []);
+
   const navigation = useNavigation();
   // for navigate updated quiz
   useEffect(() => {
     setNotificationListener(navigation);
   }, []);
+  if (!isClosed && !isClicked && !error && SHOW_INITIAL_AD) {
+    return (
+      <CenterViewContainer style={{ flex: 1 }}>
+        <ActivityIndicator />
+      </CenterViewContainer>
+    );
+  }
 
   return (
     <NativeStack.Navigator
