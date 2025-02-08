9fc427676c279bcd78394e345e9f2b6a4ff97d36
hanum <rntls123@naver.com>
2025-02-08 16:51:54 +0900

    Chore: Upgrade Expo && IOS build

--- ios/null/AppDelegate.mm
+++ ios/null/AppDelegate.mm
@@ -12,10 +12,16 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+  return [self bundleURL];
+}
+
+- (NSURL *)bundleURL
 {
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@".expo/.virtual-metro-entry"];
